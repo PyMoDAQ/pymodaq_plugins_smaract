0988443910213901f7337eedf68bf1cb10528ecc
Sébastien Weber <seba.weber@gmail.com>
2020-11-09 15:13:49 +0100

    Initial commit

---
+++ plugin_info.py
@@ -0,0 +1,11 @@
+## To modify by developper(s) of the plugin
+SHORT_PLUGIN_NAME = 'smaract'  #for instance daqmx
+package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_samarct' #to modify
+description = 'Set of PyMoDAQ plugins for Actuators from Smaract (MCS_controller, ...)'
+
+author = 'Author Name'
+author_email = 'Author email'
+
+#packages required for your plugin:
+packages_required = []
+##
