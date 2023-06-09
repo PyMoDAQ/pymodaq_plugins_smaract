a0340948c0d70e3fe23fc6302e4206c3fb8de591
Sébastien Weber <seba.weber@gmail.com>
2023-06-09 09:50:24 +0200

    modifications for pymodaq v4 compatibiltiy

--- src/pymodaq_plugins_smaract/daq_move_plugins/daq_move_SmarAct.py
+++ src/pymodaq_plugins_smaract/daq_move_plugins/daq_move_SmarAct.py
@@ -1,13 +1,11 @@
-from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, main
-from pymodaq.daq_move.utility_classes import comon_parameters
-from pymodaq.daq_utils.daq_utils import ThreadCommand
+from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, main, comon_parameters_fun
+from pymodaq.utils.daq_utils import ThreadCommand
 from easydict import EasyDict as edict
 from instrumental import instrument, Q_, list_instruments
+from instrumental.drivers.motion._smaract.scu import SCULinear
 
 psets = list_instruments(module='motion._smaract')
-
 psets_str = [f"Dev. Id{pset['id']} channel {pset['index']}" for pset in psets]
-
 
 
 class DAQ_Move_SmarAct(DAQ_Move_base):
@@ -15,9 +13,8 @@ class DAQ_Move_SmarAct(DAQ_Move_base):
 
     """
     _controller_units = ""
-
+    _epsilon = 0.002
     # find controller locators
-
 
     is_multiaxes = False
     stage_names = []
@@ -27,22 +24,11 @@ class DAQ_Move_SmarAct(DAQ_Move_base):
                  {'title': 'Frequency (Hz)', 'name': 'frequency', 'type': 'int', 'value': 450},
                  {'title': 'Amplitude (V)', 'name': 'amplitude', 'type': 'int', 'value': 100},
                  {'title': 'Max Frequency (Hz)', 'name': 'maxfreq', 'type': 'int', 'value': 18500},
-
-        ##########################################################
-        # the ones below should ALWAYS be present!!!
-        {"title": "MultiAxes:", "name": "multiaxes", "type": "group", "visible": is_multiaxes, "children": [
-            {"title": "is Multiaxes:", "name": "ismultiaxes", "type": "bool", "value": is_multiaxes},
-            {"title": "Status:", "name": "multi_status", "type": "list", "value": "Master",
-             "limits": ["Master", "Slave"],},
-            {"title": "Axis:", "name": "axis", "type": "list", "limits": stage_names},
-            ],},
-    ] + comon_parameters
+    ] + comon_parameters_fun(is_multiaxes, epsilon=_epsilon)
     ##########################################################
 
-    def __init__(self, parent=None, params_state=None):
-        super().__init__(parent, params_state)
-
-        self.controller = None
+    def ini_attributes(self):
+        self.controller: SCULinear = None
         self.settings.child("epsilon").setValue(0.002)
 
     def commit_settings(self, param):
@@ -58,65 +44,29 @@ class DAQ_Move_SmarAct(DAQ_Move_base):
     def ini_stage(self, controller=None):
         """Initialize the controller and stages (axes) with given parameters.
 
-        Parameters
-        ----------
-        controller (object): custom object of a PyMoDAQ plugin (Slave case).
-            None if only one actuator by controller (Master case)
-
-        Returns
-        -------
-        self.status: (edict) with initialization status: three fields:
-            * info: (str)
-            * controller: (object) initialized controller
-            * initialized: (bool) False if initialization failed otherwise
-                True
         """
-        try:
+        index = self.settings.child('device').opts['limits'].index(self.settings['device'])
+        self.ini_stage_init(controller, instrument(psets[index]))
 
-            self.status.update(edict(
-                info="", controller=None, initialized=False))
+        self.settings.child('units').setValue(self.controller.units)
+        self.settings.child('amplitude').setOpts(limits=list(self.controller.amplitude_limits.m_as('V')))
+        self.settings.child('frequency').setOpts(limits=list(self.controller.frequency_limits.m_as('Hz')))
+        self.settings.child('amplitude').setValue(self.controller.amplitude.m_as('V'))
+        self.settings.child('frequency').setValue(self.controller.frequency.m_as('Hz'))
+        self.settings.child('maxfreq').setValue(self.controller.max_frequency.m_as('Hz'))
 
-            # check whether this stage is controlled by a multiaxe controller
-            # (to be defined for each plugin)
-            # if multiaxes then init the controller here if Master state
-            # otherwise use external controller
-            if self.settings.child('multiaxes', 'ismultiaxes').value() \
-                    and self.settings.child('multiaxes', 'multi_status').value() == "Slave":
-                if controller is None:
-                    raise Exception('No controller has been defined externally'
-                                    ' while this axe is a slave one')
-                else:
-                    self.controller = controller
-            else:  # Master stage
-                index = self.settings.child('device').opts['limits'].index(self.settings.child('device').value())
-                self.controller = instrument(psets[index])
-                self.settings.child('units').setValue(self.controller.units)
-                self.settings.child('amplitude').setOpts(limits=list(self.controller.amplitude_limits.m_as('V')))
-                self.settings.child('frequency').setOpts(limits=list(self.controller.frequency_limits.m_as('Hz')))
-                self.settings.child('amplitude').setValue(self.controller.amplitude.m_as('V'))
-                self.settings.child('frequency').setValue(self.controller.frequency.m_as('Hz'))
-                self.settings.child('maxfreq').setValue(self.controller.max_frequency.m_as('Hz'))
+        info = ''
+        initialized = True
 
-            self.status.controller = self.controller
-            self.status.initialized = True
-
-            return self.status
-
-        except Exception as e:
-            self.emit_status(ThreadCommand("Update_Status", [str(e), "log"]))
-            self.status.info = str(e)
-            self.status.initialized = False
-
-            return self.status
+        return info, initialized
 
     def close(self):
         """Close the communication with the SmarAct controller.
         """
-
         self.controller.close()
         self.controller = None
 
-    def check_position(self):
+    def get_actuator_value(self):
         """Get the current position from the hardware with scaling conversion.
 
         Returns
@@ -133,7 +83,7 @@ class DAQ_Move_SmarAct(DAQ_Move_base):
 
         return position
 
-    def move_Abs(self, position):
+    def move_abs(self, position):
         """Move to an absolute position
 
         Parameters
@@ -150,8 +100,7 @@ class DAQ_Move_SmarAct(DAQ_Move_base):
 
         self.controller.move_to(Q_(position, self.settings['units']), 'abs')
 
-
-    def move_Rel(self, position):
+    def move_rel(self, position):
         """Move to a relative position
 
         Parameters
@@ -164,8 +113,7 @@ class DAQ_Move_SmarAct(DAQ_Move_base):
 
         self.controller.move_to(Q_(position, self.settings['units']), 'rel')
 
-
-    def move_Home(self):
+    def move_home(self):
         """Move to home and reset position to zero.
         """
         self.controller.move_home()
